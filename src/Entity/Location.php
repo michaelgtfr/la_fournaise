0c7597ec978b54acc8ccd178949a933e9ff6041b
michael <mickdu62200@gmail.com>
2021-06-13 20:42:32 +0200

    creation of Location entity file allow to provide the datas concerning the position and hour of beggin and end opening by day

---
+++ src/Entity/Location.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LocationRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=LocationRepository::class)
+ */
+class Location
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $day;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $address;
+
+    /**
+     * @ORM\Column(type="string", length=60)
+     */
+    private $city;
+
+    /**
+     * @ORM\Column(type="time")
+     */
+    private $beginHour;
+
+    /**
+     * @ORM\Column(type="time")
+     */
+    private $endTime;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDay(): ?int
+    {
+        return $this->day;
+    }
+
+    public function setDay(int $day): self
+    {
+        $this->day = $day;
+
+        return $this;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(string $address): self
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getBeginHour(): ?\DateTimeInterface
+    {
+        return $this->beginHour;
+    }
+
+    public function setBeginHour(\DateTimeInterface $beginHour): self
+    {
+        $this->beginHour = $beginHour;
+
+        return $this;
+    }
+
+    public function getEndTime(): ?\DateTimeInterface
+    {
+        return $this->endTime;
+    }
+
+    public function setEndTime(\DateTimeInterface $endTime): self
+    {
+        $this->endTime = $endTime;
+
+        return $this;
+    }
+}
