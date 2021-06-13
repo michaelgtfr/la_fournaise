146d0ddf0575b9e8de76bc481956a12387dadfbf
michael <mickdu62200@gmail.com>
2021-06-13 15:16:36 +0200

    controller from the home page (empty).

---
+++ src/Controller/homepageController.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mickd
+ * Date: 06/06/2021
+ * Time: 15:05
+ */
+
+namespace App\Controller;
+
+
+use App\Entity\Order;
+use App\Entity\User;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+class homepageController
+{
+
+}
