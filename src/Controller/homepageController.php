a0d6030eecbe981ef58423520cc26130df203b22
michael <mickdu62200@gmail.com>
2021-06-30 19:59:51 +0200

    retrieves the data concerning the position and the opening hours of the food tuck and provides them to the view

--- src/Controller/homepageController.php
+++ src/Controller/homepageController.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: mickd
+ * User: michaelgt
  * Date: 06/06/2021
  * Time: 15:05
  */
@@ -9,13 +9,30 @@
 namespace App\Controller;
 
 
-use App\Entity\Order;
-use App\Entity\User;
+use App\Entity\Location;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Twig\Environment;
 
 class homepageController
 {
+    /**
+     * @Route("/", name="app_homepage")
+     * @param EntityManagerInterface $em
+     * @param Environment $twig
+     * @return Response
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function homepage(EntityManagerInterface $em, Environment $twig)
+    {
+        $locations = $em->getRepository(Location::class)->findAll();
 
+        $render = $twig->render('homepage.html.twig', [
+            'locations' => $locations,
+        ]);
+        return new Response($render);
+    }
 }
