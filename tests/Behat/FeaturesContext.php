71a6a4d938f49aabfed4190a63848767d6869e20
michael <mickdu62200@gmail.com>
2021-07-10 23:58:09 +0200

    contains the method for the behat operation

--- tests/Behat/FeaturesContext.php
+++ tests/Behat/FeaturesContext.php
@@ -9,7 +9,84 @@
 namespace App\Tests\Behat;
 
 
-class FeaturesContext
+use App\Entity\Location;
+use Behat\Behat\Context\Context;
+use Behat\MinkExtension\Context\MinkContext;
+use DAMA\DoctrineTestBundle\Doctrine\DBAL\StaticDriver;
+use Symfony\Component\HttpKernel\KernelInterface;
+
+/**
+ * Created by PhpStorm.
+ * User: mickd
+ * Date: 10/07/2021
+ * Time: 16:13
+ */
+
+class FeaturesContext extends MinkContext implements Context
 {
+    private $em;
+
+    private $kernel;
+
+    public function __construct(KernelInterface $kernel)
+    {
+        $this->kernel = $kernel;
+        $this->em = $this->kernel->getContainer()->get('doctrine');
+    }
+
+    /**
+     * @BeforeSuite
+     */
+    public static function beforeSuite()
+    {
+        StaticDriver::setKeepStaticConnections(true);
+    }
+
+    /**
+     * @BeforeScenario
+     */
+    public function beforeScenario()
+    {
+        StaticDriver::beginTransaction();
+    }
+
+    /**
+     * @AfterScenario
+     */
+    public function afterScenario()
+    {
+        StaticDriver::rollBack();
+    }
+
+    /**
+     * @AfterSuite
+     */
+    public static function afterSuite()
+    {
+        StaticDriver::setKeepStaticConnections(false);
+    }
+
+    public function locationData()
+    {
+        $location = new Location();
+        $location->setDay(1);
+        $location->setAddress('testAddress');
+        $location->setCity('testCity');
+        $location->setBeginHour(new \DateTime());
+        $location->setEndTime(new \DateTime());
+
+        $manager = $this->em->getManager();
+        $manager->persist($location);
+        $manager->flush();
+    }
+
+    /**
+     * @When I visit the web site
+     */
+    public function iVisitTheWebSite()
+    {
+        $this->locationData();
+        $this->iAmOnHomepage();
+    }
 
 }
