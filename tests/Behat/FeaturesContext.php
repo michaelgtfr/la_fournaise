709b9154b4dbb235821a43478d4e2d4474864657
michael <mickdu62200@gmail.com>
2021-07-10 23:29:23 +0200

    setting up to tests for the display page

---
+++ tests/Behat/FeaturesContext.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mickd
+ * Date: 10/07/2021
+ * Time: 16:33
+ */
+
+namespace App\Tests\Behat;
+
+
+class FeaturesContext
+{
+
+}
---
+++ tests/Controller/HomepageControllerTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mickd
+ * Date: 10/07/2021
+ * Time: 01:13
+ */
+
+namespace App\Tests\Controller;
+
+
+class HomepageControllerTest
+{
+
+}
