5f6f31860394a66e8e24b5c177adbb269c9a9e4a
michael <mickdu62200@gmail.com>
2021-07-18 00:15:55 +0200

    add of js and jquery code allowing the setting up of the map and the marker

--- assets/homepage.js
+++ assets/homepage.js
@@ -1,1 +1,25 @@
 import './styles/homepage.scss';
+import $ from 'jquery';
+const Marker = require( './Components/Marker');
+
+$(document).ready( function () {
+    let marker = new Marker();
+
+    let day = new Date().getDay();
+
+    let location = document.getElementById('day-'+day);
+
+    let message = location.querySelector('.hour').textContent + location.querySelector('.address').textContent;
+
+    marker.markerPoint(location.dataset.longitude, location.dataset.latitude, message);
+
+    $(".card").click(function () {
+        let longitude = $(this).data("longitude");
+        let latitude = $(this).data("latitude");
+        let message = $(this).find(".hour").text() + $(this).find(".address").text();
+
+        $('.leaflet-marker-icon').remove();
+        $('.leaflet-marker-shadow').remove();
+        marker.markerPoint(longitude, latitude, message);
+    });
+});
