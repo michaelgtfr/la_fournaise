fc0e8673f3ab564dbd29d389bef491fce2d96b80
michael <mickdu62200@gmail.com>
2021-07-28 22:52:28 +0200

    add the style of the day part

--- assets/homepage.js
+++ assets/homepage.js
@@ -2,18 +2,30 @@ import './styles/homepage.scss';
 import $ from 'jquery';
 const Marker = require( './Components/Marker');
 
-$(document).ready( function () {
+$(document).ready( function (e) {
+
     let marker = new Marker();
 
     let day = new Date().getDay();
+
+    let marker_number_total_call = 1;
+
+    let leaflet = document.querySelector('#mapid');
+    leaflet.setAttribute("data-number-marker-total-call", marker_number_total_call );
 
     let location = document.getElementById('day-'+day);
 
     let message = location.querySelector('.hour').textContent + location.querySelector('.address').textContent;
 
+
     marker.markerPoint(location.dataset.longitude, location.dataset.latitude, message);
 
-    $(".card").click(function () {
+    $(".card").click(function (e) {
+        e.preventDefault();
+
+        marker_number_total_call += 1;
+        leaflet.setAttribute("data-number-marker-total-call",  marker_number_total_call);
+
         let longitude = $(this).data("longitude");
         let latitude = $(this).data("latitude");
         let message = $(this).find(".hour").text() + $(this).find(".address").text();
