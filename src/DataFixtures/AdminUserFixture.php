19897c4903a4a1a1e5f640316fc4846767733930
michael <mickdu62200@gmail.com>
2021-08-21 12:52:10 +0200

    contain the fixture of the administrator entityé

--- src/DataFixtures/AdminUserFixture.php
+++ src/DataFixtures/AdminUserFixture.php
@@ -9,7 +9,35 @@
 namespace App\DataFixtures;
 
 
-class AdminUserFixture
-{
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
+class AdminUserFixture extends Fixture
+{
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setName('nameTest');
+        $user->setNameOrderWithdrawal('nameTest');
+        $user->setPassword($this->encoder->encodePassword($user,'1'));
+        $user->setEmail('test_email@gmail.com');
+        $user->setNumberCellphone('0700000000');
+        $user->setConfirmationAccount(1);
+        $user->setConfirmationKey(1);
+        $user->setRoles([
+            "ROLE_ADMIN"
+        ]);
+
+        $manager->persist($user);
+        $manager->flush();
+    }
 }
