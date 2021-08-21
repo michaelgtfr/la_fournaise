46b811099835874f98804dc6ec677e5aa89e2eaa
michael <mickdu62200@gmail.com>
2021-08-21 12:51:18 +0200

    contains the Fixture of ApplicationInformation Entity

--- src/DataFixtures/ApplicationInformationFixture.php
+++ src/DataFixtures/ApplicationInformationFixture.php
@@ -9,7 +9,20 @@
 namespace App\DataFixtures;
 
 
-class AppliccationInformationFixture
-{
+use App\Entity\ApplicationInformation;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
 
+class ApplicationInformationFixture extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $applicationInformation = new ApplicationInformation();
+        $applicationInformation->setPhoneNumberApplication(0700000000);
+        $applicationInformation->setFacebookApplication('facebook/test');
+        $applicationInformation->setEmailApplication('email_test@gmail.com');
+
+        $manager->persist($applicationInformation);
+        $manager->flush();
+    }
 }
