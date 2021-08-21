cfcb9b6d7d9e6a329bb49281bc8fe83d5dd7e70b
michael <mickdu62200@gmail.com>
2021-08-21 12:40:37 +0200

    allowing to test the display of the AdminDashboard page

--- tests/Controller/AdminDashboardControllerTest.php
+++ tests/Controller/AdminDashboardControllerTest.php
@@ -9,7 +9,45 @@
 namespace App\Tests\Controller;
 
 
-class AdminDashboardControllerTest
+use App\DataFixtures\AdminUserFixture;
+use App\DataFixtures\ApplicationInformationFixture;
+use App\Entity\User;
+use Doctrine\Common\DataFixtures\Purger\ORMPurger;
+use Liip\TestFixturesBundle\Test\FixturesTrait;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class AdminDashboardControllerTest extends WebTestCase
 {
+    use FixturesTrait;
+
+    protected $client = null;
+
+    protected function dataFixture()
+    {
+        $this->loadFixtures([
+            AdminUserFixture::class,
+            ApplicationInformationFixture::class
+        ]);
+    }
+
+    public function tearDown(): void
+    {
+        $purger = new ORMPurger(self::$container->get('doctrine')->getManager());
+        $purger->purge();
+        parent::tearDown();
+    }
+
+    public function testAdminPageDisplay()
+    {
+        $this->client = static::createClient();
+        $this->dataFixture();
+
+        $user = self::$container->get('doctrine')->getRepository(User::class)->findOneBy(['email' => 'test_email@gmail.com']);
+        $this->client->loginUser($user);
+
+        $crawler = $this->client->request('GET', 'admin/dashboardadmin');
+
+        $this->assertEquals('Mes données', $crawler->filter('h2')->text());
+    }
 
 }
