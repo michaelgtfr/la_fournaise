18964941237876941282c62e46876034be134dc7
michael <mickdu62200@gmail.com>
2021-08-22 17:33:07 +0200

    creation of the functionality allowing the display of coordinates and timetables

---
+++ src/Controller/CoordinatesAndTimetablesController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mickd
+ * Date: 21/08/2021
+ * Time: 17:47
+ */
+
+namespace App\Controller;
+
+
+class CoordinatesAndTimetablesController
+{
+
+}
---
+++ tests/Controller/CoordinatesAndTimetablesControllerTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mickd
+ * Date: 22/08/2021
+ * Time: 11:41
+ */
+
+namespace App\Tests\Controller;
+
+
+class CoordinatesAndTimetablesControllerTest
+{
+
+}
