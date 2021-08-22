55d4b106892af3ae6ced1e8b3b9ec08278b0c907
michael <mickdu62200@gmail.com>
2021-08-22 17:40:09 +0200

    creation of the controller allowing the recover of location datas and transmit to the view

--- tests/Controller/CoordinatesAndTimetablesControllerTest.php
+++ tests/Controller/CoordinatesAndTimetablesControllerTest.php
@@ -9,7 +9,42 @@
 namespace App\Tests\Controller;
 
 
-class CoordinatesAndTimetablesControllerTest
-{
+use App\DataFixtures\AdminUserFixture;
+use App\DataFixtures\AppFixtures;
+use App\Entity\User;
+use Doctrine\Common\DataFixtures\Purger\ORMPurger;
+use Liip\TestFixturesBundle\Test\FixturesTrait;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
+class CoordinatesAndTimetablesControllerTest extends WebTestCase
+{
+    use FixturesTrait;
+
+    protected function dataFixture()
+    {
+        $this->loadFixtures([
+            AdminUserFixture::class,
+            AppFixtures::class,
+        ]);
+    }
+
+    public function tearDown(): void
+    {
+        $purger = new ORMPurger(self::$container->get('doctrine')->getManager());
+        $purger->purge();
+        parent::tearDown();
+    }
+
+    public function testAdminCoordinatesAndTimetablesPageDisplay()
+    {
+        $this->client = static::createClient();
+        $this->dataFixture();
+
+        $user = self::$container->get('doctrine')->getRepository(User::class)->findOneBy(['email' => 'test_email@gmail.com']);
+        $this->client->loginUser($user);
+
+        $crawler = $this->client->request('GET', 'admin/coordonneesethoraires');
+
+        $this->assertEquals('Les coordonnées et horaires:', $crawler->filter('h5')->text());
+    }
 }
