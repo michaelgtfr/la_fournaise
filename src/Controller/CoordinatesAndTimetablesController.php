c03eab04391cc2551d0c121020f7ce7d99f6b344
michael <mickdu62200@gmail.com>
2021-08-22 19:01:37 +0200

    recover the location datas and the transmit to th view

--- src/Controller/CoordinatesAndTimetablesController.php
+++ src/Controller/CoordinatesAndTimetablesController.php
@@ -9,7 +9,31 @@
 namespace App\Controller;
 
 
+use App\Entity\Location;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
+
 class CoordinatesAndTimetablesController
 {
+    /**
+     * @Route("/admin/coordonneesethoraires", name="app_admin_coordinates_and_timetables")
+     * @param EntityManagerInterface $em
+     * @param Environment $twig
+     * @return Response
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function coordinatesAndTimeTable(EntityManagerInterface $em, Environment $twig)
+    {
+        $locations = $em->getRepository(Location::class)->findAll();
 
+        $render = $twig->render('coordinatesAndTimetables.html.twig', [
+            'locations' => $locations,
+        ]);
+
+        return new Response($render);
+    }
 }
