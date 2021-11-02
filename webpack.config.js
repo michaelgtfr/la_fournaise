c58b160174c920d54dae2a8a27f6fcb105785607
michael <mickdu62200@gmail.com>
2021-11-02 16:34:36 +0100

    automatic update

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,8 @@ Encore
     .addEntry('coordinatesAndTimetables', './assets/coordinatesAndTimetables.js')
     .addEntry('contact', './assets/contact.js')
     .addEntry('clientDashboard', './assets/clientDashboard.js')
+    .addEntry('register', './assets/register.js')
+    .addEntry('updateCoordinatesAndTimetables', './assets/updateCoordinatesAndTimetables.js')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
