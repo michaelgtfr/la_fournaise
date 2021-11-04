d1a99651b760241942134a2bd71b2491b1e57da5
michael <mickdu62200@gmail.com>
2021-11-04 11:24:21 +0100

    create the service with a processing picture file

---
+++ src/Service/ProcessingFiles.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mickd
+ * Date: 04/11/2021
+ * Time: 06:46
+ */
+
+namespace App\Service;
+
+
+use App\Entity\PictureProduct;
+use Doctrine\ORM\EntityManagerInterface;
+
+class ProcessingFiles
+{
+    /**
+     * Move the image to the img Avatar folder and change its name, protection against the fault UPLOAD
+     * @param $linkFile
+     * @param $extensionFile
+     * @param $folder
+     * @return string
+     */
+    public function processingFiles($linkFile, $extensionFile, $folder)
+    {
+        //creation of the new name
+        $datePicture = date('Y_m_d_H_i_s');
+        $idUnique = uniqid();
+        $namePhoto = "{$datePicture}-{$idUnique}.{$extensionFile}";
+
+        //transfer
+        $transferFile ="img/$folder/$namePhoto";
+        move_uploaded_file($linkFile, $transferFile);
+
+        return $namePhoto;
+    }
+
+    /**
+     * Delete the file in the img folder and the bdd
+     * @param $folder
+     * @param $name
+     * @param $extension
+     * @param EntityManagerInterface $em
+     */
+    public function deletePictureProduct($folder , $name, $extension, EntityManagerInterface $em)
+    {
+        unlink('img/'.$folder.'/'.$name.'.'.$extension);
+    }
+}
